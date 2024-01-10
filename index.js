7d87a49cea1fa956032356a6928fa17f172f5fb3
ferdinardrain <144677958+ferdinardrain@users.noreply.github.com>
2024-01-10 18:46:23 +0000

    Add files via upload
     first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+ScrollReveal().reveal('body', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer(el));
